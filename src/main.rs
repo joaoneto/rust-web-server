b1b5c949767e1533240d384d1ba676849c55cf5f
João Neto <joaopintoneto@gmail.com>
2020-09-12 02:40:25 -0300

    added tokio

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,59 @@
-// use std::io::{
-//     prelude::*,
-//     ErrorKind,
-// };
-// use std::net::{
-//     TcpStream,
-//     TcpListener,
-//     Shutdown,
-// };
-// use std::time::Duration;
+// mod lib;
+// use lib::{App, Request, Response};
+use std::{env, error::Error};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::prelude::*;
+use std::net::Shutdown;
 
-mod lib;
-use lib::App;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "0.0.0.0:9000".to_string());
+    let mut server = TcpListener::bind(&addr).await?;
 
-fn main() {
-    let mut my_app = App::new("lala".to_string());
-    my_app.parse_request("GET /index.html HTTP/1.1".to_string());
+    println!("listening: {}", addr);
 
-    my_app.middleware(move | req, _res | {
-        println!("Middleware 1 {:?}", req);
-    });
-    my_app.middleware(move | req, _res | {
-        println!("Middleware 2 {:?}", req);
-    });
+    loop {
+        let (stream, _) = server.accept().await?;
 
-    my_app.call();
-    println!("{}", my_app);
+        println!("accepted {:?}", stream.peer_addr());
 
-    // let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
-
-    // // implementar async IO (Epoll, Kqueue and IOCP)
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(s) => {
-    //             handle_request(s);
-    //         }
-    //         Err(e) => panic!("encountered IO error: {}", e),
-    //     }
-    // }
+        tokio::spawn(async move {
+            if let Err(e) = process(stream).await {
+                eprintln!("failed to process connection; err = {}", e);
+            }
+        });
+    }
 }
 
-// fn handle_request(mut stream: TcpStream) {
-//     let mut buffer = [0; 128];
-//     let mut raw_request = String::new();
+async fn process(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut buffer = [0; 2048];
+    let mut raw_request = String::new();
 
-//     stream.set_nodelay(true).expect("failed to set nodelay");
-//     stream.set_nonblocking(false).expect("failed to set nonblocking");
-//     stream.set_read_timeout(Some(Duration::new(0, 100))).expect("failed to set read timeout");
+    loop {
+        match stream.read(&mut buffer).await {
+            Ok(size) if size == 0 => {
+                // println!("socket closed");
+                break;
+            },
+            Ok(size) => {
+                let chunk = String::from_utf8_lossy(&buffer[0..size]);
+                raw_request.push_str(&chunk);
+            },
+            Err(e) => {
+                eprintln!("failed to read from socket; err = {:?}", e);
+                return Err(e.into());
+            },
+        };
 
-//     loop {
-//         match stream.read(&mut buffer[..]) {
-//             Ok(size) => {
-//                 let chunk = String::from_utf8_lossy(&buffer[0..size]);
-//                 raw_request.push_str(&chunk);
-//                 println!("Received: {} {:?}", size, chunk);
+        if let Err(e) = stream.write_all(b"HTTP/1.1 200 OK\r\nContent-Type: application/json; charset=utf-8\r\n\r\n{ \"ok\": true }\r\n\r\n").await {
+            eprintln!("failed to write to socket; err = {:?}", e);
+        }
 
-//                 if size == 0 {
-//                     break;
-//                 }
-//             }
-//             Err(e) if e.kind() == ErrorKind::WouldBlock => {
-//                 eprintln!("WouldBlock: {}", e);
-//                 break;
-//             }
-//             Err(e) if e.kind() == ErrorKind::Interrupted => {
-//                 println!("Interrupted: {}", e);
-//                 break;
-//             }
-//             Err(e) => {
-//                 println!("Error: {}", e);
-//                 break;
-//             }
-//         }
-//     }{}
-    
-//     println!("{}", raw_request);
+        stream.flush().await?;
+        stream.shutdown(Shutdown::Write)?;
+    };
 
-//     stream.write_all(b"HTTP/1.1 200 OK\r\nContent-Type: application/json; charset=utf-8\r\n\r\n{ \"ok\": true }\r\n\r\n").unwrap();
-//     stream.flush().unwrap();
-//     stream.shutdown(Shutdown::Both).expect("shutdown call failed");
-
-// }
+    Ok(())
+}
